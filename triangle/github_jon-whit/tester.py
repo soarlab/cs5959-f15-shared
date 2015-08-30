4763f32bfb723011fdc030695782aaab12b7794e
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2015-08-30 15:04:25 -0600

    Jonathan Whitaker - commit of triangle classifier and supporting file.

---
+++ triangle/github_jon-whit/tester.py
@@ -0,0 +1,39 @@
+import subprocess
+
+def run_test(test, expected):
+    output = subprocess.check_output(test, shell=True)
+
+    if output != expected:
+        print ("FAILED TEST: '%s'\n\tEXPECTED: %s\r\tRETURNED: %s\n") % (test, expected, output)
+
+if __name__ == "__main__":
+    subprocess.call(['make'], shell=True)
+
+    # test 3 points on a line
+    run_test("./a.out 1 2 3 4 5 6", "not a triangle\n")
+    run_test("./a.out 1    2    3    4    5    6", "not a triangle\n")
+    run_test("./a.out 0 0 0 0 0 0", "not a triangle\n")
+
+    # test isosceles triangles
+    run_test("./a.out 1073741823 0 -1073741823 0 0 1", "isosceles obtuse\n")
+    run_test("./a.out -1073741823 0 1073741823 0 0 1", "isosceles obtuse\n")
+    run_test("./a.out 0 0 0 1073741823 1073741823 0", "isosceles right\n")
+
+    # test scalene triangles
+    run_test("./a.out 0 0 0 1073741823 1073741822 0", "scalene right\n")
+    run_test("./a.out 0 0 -1 1073741823 1073741823 0", "scalene obtuse\n")
+    run_test("./a.out 0 0 1 1073741823 1073741823 0", "scalene acute\n")
+
+    # test illegal input
+    run_test("./a.out", "error\n")
+    run_test("./a.out 1 2 3 4 56", "error\n")
+    run_test("./a.out 1 2 3 4 5 6 7", "error\n")
+    run_test("./a.out 1  2  3  4  56", "error\n")
+    run_test("./a.out 1  2  3  4  5  6  7", "error\n")
+    run_test("./a.out 1073741824 0 0 0 1 2", "error\n")
+    run_test("./a.out -1073741824 0 0 0 1 2", "error\n")
+    run_test("./a.out a b c d e f", "error\n")
+    run_test("./a.out a3 1 2 3 4 5", "error\n")
+    run_test("./a.out 1 as3df 2 3 4 5", "error\n")
+    run_test("./a.out 1 2 3 4asdf 5 6", "error\n")
+    run_test("./a.out 1 2 3 4 5 jon", "error\n")
---
+++ triangle/github_jon-whit/triangle.c
@@ -0,0 +1,153 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define DEBUG 0
+
+typedef struct {
+    long int x;
+    long int y;
+} point;
+
+/**
+ * Determines if 3 points lie on the same line.
+ *
+ * If the 3 points lie on the same line then they will have the same slope. Otherwise they
+ * make a triangle.
+ *
+ * Comparison come from: http://math.stackexchange.com/a/405981
+ */
+int make_line(point *p1, point *p2, point *p3) {
+    if ( (p2->y - p1->y)*(p3->x - p2->x) == (p3->y - p2->y)*(p2->x - p1->x) )
+        return 1;
+    else
+ 	return 0;
+}
+
+/**
+ * Classsifies the Triangle given by the 3 points that were specified.
+ * 
+ * A Triangle is classified as follows:
+ *   1. Scalene | Equilateral | Isosceles
+ *   2. Acute   | Obtuse      | Right
+ *   3. Not A Triangle
+ */
+void classify_triangle(point *p1, point *p2, point *p3) {
+    
+    if ( DEBUG )
+        printf("Classifying Triangle with points: (%li, %li), (%li, %li), (%li, %li)...\n", p1->x, p1->y, p2->x, p2->y, p3->x, p3->y);
+    
+    long int l1 = (p2->x - p1->x)*(p2->x - p1->x) + (p2->y - p1->y)*(p2->y - p1->y);    
+    long int l2 = (p3->x - p2->x)*(p3->x - p2->x) + (p3->y - p2->y)*(p3->y - p2->y);
+    long int l3 = (p3->x - p1->x)*(p3->x - p1->x) + (p3->y - p1->y)*(p3->y - p1->y);
+    
+    if ( DEBUG ) {
+        printf("l1: %li\n", l1);
+        printf("l2: %li\n", l2);
+        printf("l3: %li\n", l3);
+    }
+
+    /* If the three points lie on the same line then they will have the same slope, otherwise
+     * they make a triangle.
+     *
+     * Comparison comes from: http://math.stackexchange.com/a/405981
+     */
+    if ( make_line(p1, p2, p3) ) {
+        printf("not a triangle");
+	return;
+    }
+
+    if ( l1 == l2 == l3 ) {
+        printf("equilateral "); 
+    } else if ( l1 == l2 | l1 == l3 | l2 == l3 ) {
+        printf("isosceles ");
+    } else {
+        printf("scalene ");
+    }
+
+    // find the longest side length
+    long int a = l2, b = l3, c = l1;
+    if ( l2 > c && l2 > l3 ) {
+        a = l1;
+        b = l3;
+        c = l2;
+    }
+    else if (l3 > c && l3 > l2 ) {
+        a = l1;
+        b = l2;
+        c = l3;
+    }
+
+    // classify according to angle by using Pythagorean's theorem
+    // here a = a^2, b = b^2, and c = c^2 (as you can see from above)
+    if ( a + b == c )
+        printf("right");
+    else if ( a + b > c )
+    	printf("acute");
+    else
+  	printf("obtuse");
+}
+
+/**
+ * Validates the input arguments according to the supplied criterion for this assignment.
+ * 
+ * Each of the six arguments 
+ */
+int valid_input(int argc, char **argv) {
+    
+    // if less or more than 6 arguments were supplied, then print an error message.
+    if ( argc != 7 ) {
+        return 0;
+    }    
+
+    /* check if each argument is an integer and within the range specified by the
+     * assignment
+     * 
+     * For more information see: http://www.cplusplus.com/reference/cstdlib/strtol/
+     */
+    int i;
+    for (i = 1; i < 7; i++) {
+        char * endPtr;
+        long int res = strtol(argv[i], &endPtr, 10);
+	if ( (!res || errno == ERANGE || *endPtr != '\0') && (*argv[i] != '0') )
+            return 0; 
+        else if ( res > 1073741823 || res < -1073741823 )
+     	    return 0;
+    }
+   
+    // otherwise each of the arguments was valid
+    return 1; 
+}
+
+/**
+ * Main entry point for the Triangle classifier.
+ */
+int main(int argc, char **argv) {
+
+    if ( !valid_input(argc, argv) ) {
+        printf("error\n");
+        return 0;
+    }    
+ 
+    // otherwise all of the supplied arguments were valid. pass the points through
+    // to the Triangle classifier.
+    long int x1, x2, x3, y1, y2, y3;
+
+    x1 = strtol(argv[1], NULL, 10), y1 = strtol(argv[2], NULL, 10);
+    x2 = strtol(argv[3], NULL, 10), y2 = strtol(argv[4], NULL, 10);
+    x3 = strtol(argv[5], NULL, 10), y3 = strtol(argv[6], NULL, 10);
+    point p1 = {x1, y1};
+    point p2 = {x2, y2};
+    point p3 = {x3, y3};
+
+    if ( DEBUG ) {
+        printf("p1: (%li, %li)\n", p1.x, p1.y);
+        printf("p2: (%li, %li)\n", p2.x, p2.y);
+        printf("p3: (%li, %li)\n", p3.x, p3.y);
+     }
+
+    classify_triangle(&p1, &p2, &p3);
+    printf("\n");
+
+    return 0;
+}
