03026e216f1d5b2520dcd5ddb68be46ff4823c5b
Larry-mbp <Larry-mbp@Larry-mbps-MacBook-Pro.local>
2015-08-31 09:12:14 -0600

    Submitting final draft.

--- triangle/github_jdoge/triangle_classifier.h
+++ triangle/github_jdoge/triangle_classifier.h
@@ -20,8 +20,6 @@ int DuplicatePointsCheck(struct triangle t);
 int ColinearityCheck(struct triangle t);
 __int64_t CalculateLength(struct point point1, struct point point2);
 int CountEqualLengths(__int64_t arr[]); 
-int CompareFunction(const void* a, const void* b);
 __int64_t FindGCD(__int64_t number1, __int64_t number2);
-
-
 void BubbleSort(__int64_t a[], int array_size);
+int CompareFunction(const void* a, const void* b);
