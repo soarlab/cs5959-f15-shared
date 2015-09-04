fe85e69d69c5da7b018553e397f42982a24bd3c9
git <rolyat.wilson@gmail.com>
2015-09-04 00:11:59 -0600

    first iterations of improvement...still waiting for CRs

--- triangle/github_tjhooker33/triangle_analyzer.h
+++ triangle/github_tjhooker33/triangle_analyzer.h
@@ -1,4 +1,4 @@
-/*******************************************************************************************
+/*
  * AUTHOR :     Taylor Wilson
  * UID :        U0323893
  *
@@ -7,27 +7,16 @@
  * PURPOSE :    Written for CS 5959 Writing Solid Code, Fall 2015
  *              University of Utah
  *
- *******************************************************************************************
  */
 
-#include <assert.h>
 #include <ctype.h>
-#include <errno.h>
-#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 
-// Colors for standard out
-#define KCYN  "\x1B[36m"
-#define KGRN  "\x1b[32m"
-#define KMAG  "\x1b[35m"
-#define KRED  "\x1B[31m"
-#define RESET "\033[0m"
-
-#define POSITIVE 0
-#define NEGATIVE -1
+#define ARG_COUNT 7
+#define MAX_RANGE 1073741823
+#define MIN_RANGE -1073741823
 
 #define ERROR() {     \
     printf("error\n");\
@@ -39,133 +28,22 @@
   }
 
 typedef struct coordinate {
-  uint64_t x;
-  uint64_t y;
-  int x_sign;
-  int y_sign;
+  long x;
+  long y;
 } coordinate;
 
-typedef struct slope {
-  uint64_t rise;
-  uint64_t run;
-  int sign;
-} slope;
-
-typedef struct length_slope {
-  uint64_t length;
-  slope m;
-} length_slope;
-
-typedef struct euclid_tuple {
-  uint64_t x;
-  uint64_t y;
-  uint64_t d;
-} euclid_tuple;
-
-// Prints points A,B,C to stdout
-void print_points(void);
-
-// Program must run with 7 arguements (first is executable).
-// Returns -1 arg count is not 7, otherwise returns 0.
 int check_arg_count(int argc);
-
-// Returns -1 if any two points ABC are equal, otherwise returns 0.
-int check_duplicate_points(void);
-
-// Checks if points ABC form a diagonal line.
-// Returns -1 if ABC form a line, otherwise returns 0.
-int check_equal_slopes(slope a, slope b, slope c);
-
-// Each character of each input string must be a number.
-// Only exception is a '-' sign as the left most character.
-// Returns -2 on error, -1 if input is negative, 0 if input is non-negative.
-int check_input_format(char *str);
-
-// Absolute value of input must not exceed 2^30-1.
-// Returns -1 if input is out of range, otherwise returns 0.
-int check_range(uint64_t num);
-
-// Checks if points ABC represent a valid triangle.
-// Returns -1 if not, otherwise returns 0.
-int check_triangle(void);
-
-// Checks if points ABC form a horizontal or vertical line.
-// Returns -1 if ABC form a line, otherwise returns 0.
-int check_vert_horz_lines(void);
-
-// Computes the length or slope between p1 and p2. The length is
-// computed by making a call to *compute* providing X or Y coordinates
-// as inputs. The slope is computed by making a call to *compute* providing
-// deltaY or deltaX parameters as inputs.
-// Example:
-// 		rise = compute(Y1, Y2);
-//	    run  = compute(X1, X2);
-uint64_t compute(uint64_t p1, uint64_t p2);
-
-// Determines whether the slope value between 2 points is negative or non-negative.
-// Returns -1 if slope is negative, otherwise returns 0.
-int compute_slope_sign(uint64_t x1, uint64_t x2, uint64_t y1, uint64_t y2);
-
-// Creates a point of the triangle based on x,y coordinate pair and signs.
-// Parameters  :
-//		point  : 1 for point A, 2 for point B, 3 for point C of the triangle ABC 
-// 		x_sign : sign flag for unsigned x value
-//      y_sign : sign flag for unsigned y value
-//      *x     : string containing numeric value of x coordinate
-//      *y     : string containing numeric value of y coordinate
-// Returns -1 if an error occurs, otherwise returns 0.
-int create_point(int point, int x_sign, int y_sign, char *x, char *y);
-
-// Constructs a set of points ABC representing a triangle from the input arguments.
-// Returns -1 if an error occurs, otherwise returns 0.
+int check_input_format(char *num);
+int create_point(char *num, long *component);
+int check_range(long *num);
 int create_triangle(char *argv[]);
-
-// Recursively finds the GCD of inputs a and b.
-// Returns the GCD as a tuple* (x, y, d) such that
-//   d = GCD(a, b) and ax + by = d
-// The returned tuple is dynamically allocated. The caller is 
-// responsible for freeing the returned tuple.
-euclid_tuple* extended_euclid(uint64_t a, uint64_t b);
-
-// Because this program uses unsigned values to represent points in
-// a 4 quadrant plane, points must be translated to the same quadrant
-// in order to find an accurate length or slope. This function determines
-// which quadrant inputs P1 and P2 are in, makes any necessary translations, and
-// then computes the length and slope between P1 and P2.
-// 
-// Returns a struct* containing the length and slope values. The struct is
-// dynamically allocated; it is the responsibility of the caller to free it.
-length_slope* find_length_and_slope(coordinate P1, coordinate P2);
-
-// Returns an integer representing the x,y quadrant the point is in.
-int find_quadrant(coordinate pair);
-
-// Returns 1 if sides AB, AC, CB of triangle ABC represent an equilateral triangle.
-// Otherwise returns 0.
-int is_equilateral(length_slope AB, length_slope AC, length_slope CB);
-
-// Returns 1 if sides AB, AC, CB of triangle ABC represent an isosceles triangle.
-// Otherwise returns 0.
-int is_isosceles(length_slope AB, length_slope AC, length_slope CB);
-
-// Returns 1 if sides AB, AC, CB of triangle ABC represent a scalene triangle.
-// Otherwise returns 0.
-int is_scalene(length_slope AB, length_slope AC, length_slope CB);
-
-// Returns 1 if sides AB, AC, CB of triangle ABC represent an acute triangle.
-// Otherwise returns 0.
-int is_acute(length_slope AB, length_slope AC, length_slope CB);
-
-// Returns 1 if sides AB, AC, CB of triangle ABC represent a right triangle.
-// Otherwise returns 0.
-int is_right(length_slope AB, length_slope AC, length_slope CB);
-
-// Returns 1 if sides AB, AC, CB of triangle ABC represent an obtuse triangle.
-// Otherwise returns 0.
-int is_obtuse(length_slope AB, length_slope AC, length_slope CB);
-
-// Reduces the slope represented by the inputs rise/run.
-void reduce_slope(uint64_t *rise, uint64_t *run);
-
-// Prints the classification of the triangle ABC based on sides AB, AC, BC.
-void print_classification(length_slope AB, length_slope AC, length_slope CB);
+long long compute_distance(coordinate *p1, coordinate *p2);
+void swap_triangle_sides(long long *i, long long *j);
+int check_colinearity(void);
+void compute_triangle_sides(void);
+int is_isosceles(void);
+int is_scalene(void);
+int is_acute(void);
+int is_right(void);
+int is_obtuse(void);
+void print_classification(void);
--- triangle/github_tjhooker33/triangle_test.py
+++ triangle/github_tjhooker33/triangle_test.py
@@ -1,4 +1,4 @@
- # *******************************************************************************************
+ # 
  # AUTHOR :     Taylor Wilson
  # UID :        U0323893
  # 
@@ -6,9 +6,9 @@
  # 
  # PURPOSE :    Written for CS 5959 Writing Solid Code, Fall 2015
  #              University of Utah
- # 
- # *******************************************************************************************
- 
+ #
+
+import re
 import os
 from subprocess import Popen, PIPE
 import unittest
@@ -29,129 +29,157 @@ class Types:
 
 class TestTriangle(unittest.TestCase):
 
+  pattern  = '(((scalene|isosceles|equilateral) (acute|obtuse|right))|not a triangle|error)\n'
+  triangle = None
+
   def setUp(self):
     pass
 
   def tearDown(self):
     pass
 
+  # ============================================================================================================
+  # Assertion Helpers: 
+  # ============================================================================================================
+
+  def assertSubType(self, subType):
+    self.assertTrue(re.match(self.pattern, self.triangle.output()))
+    if subType == Types.ACU:
+      self.assertEqual(self.triangle.subType, Types.ACU)
+    elif subType == Types.RIG:
+      self.assertEqual(self.triangle.subType, Types.RIG)
+    elif subType == Types.OBT:
+      self.assertEqual(self.triangle.subType, Types.OBT)
+
+  def assertIsosceles(self, subType):
+    self.triangle.build()
+    self.assertEqual(self.triangle.priType, Types.ISO)
+    self.assertSubType(subType)
+
+  def assertScalene(self, subType):
+    self.triangle.build()
+    self.assertEqual(self.triangle.priType, Types.SCA)
+    self.assertSubType(subType)
+
+  def assertError(self):
+    self.triangle.build()
+    self.assertTrue(re.match(self.pattern, self.triangle.output()))
+    self.assertEqual(self.triangle.output(), Types.ERR)
+
+  def assertNotATriangle(self):
+    self.triangle.build()
+    self.assertTrue(re.match(self.pattern, self.triangle.output()))
+    self.assertEqual(self.triangle.output(), Types.NAT)
+
+  # ============================================================================================================
+  # Test Cases for bad inputs
+  # ============================================================================================================
+
   def testNoArgs(self):
-    tri = Triangle('0 0 0 0 0 0'.split())
-    tri.erasePoints()
-    tri.build()
-    self.assertEqual(tri.output(), Types.ERR)
+    self.triangle = Triangle('0 0 0 0 0 0'.split())
+    self.triangle.erasePoints()
+    self.assertError()
 
   def testExcessArgs(self):
-    tri = Triangle('0 0 0 0 0 0'.split())
-    tri.addPoint('1')
-    tri.build()
-    self.assertEqual(tri.output(), Types.ERR)
+    self.triangle = Triangle('0 0 0 0 0 0'.split())
+    self.triangle.addPoint('1')
+    self.assertError()
 
   def testMissingArgs(self):
-    tri = Triangle('0 0 0 0 0 0'.split())
-    tri.removePoint('0')
-    tri.build()
-    self.assertEqual(tri.output(), Types.ERR)
+    self.triangle = Triangle('0 0 0 0 0 0'.split())
+    self.triangle.removePoint('0')
+    self.assertError()
 
   def testInvalidChars(self):
-    tri = Triangle('0 0 0 0* 0 0'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.ERR)
+    self.triangle = Triangle('0 0 0 0* 0 0'.split())
+    self.assertError()
 
   def testNegativeSign(self):
-    tri = Triangle('-1 -1 0 0 -1 1'.split())
-    tri.build()
-    self.assertEqual(tri.typeA, Types.ISO)
-    self.assertEqual(tri.typeB, Types.RIG)
+    self.triangle = Triangle('-1 -1 0 0 -1 1'.split())
+    self.assertIsosceles(Types.RIG)
 
   def testLeadingZeros(self):
-    tri = Triangle('00000000000000000000001 0 0 000 0000000 01'.split())
-    tri.build()
-    self.assertEqual(tri.typeA, Types.ISO)
-    self.assertEqual(tri.typeB, Types.RIG)
+    self.triangle = Triangle('00000000000000000000001 0 0 000 0000000 01'.split())
+    self.assertIsosceles(Types.RIG)
 
   def testOverflowArgs(self):
-    tri = Triangle('1 2 3 4 0 1073741824'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.ERR)
+    self.triangle = Triangle('1 2 3 4 0 1073741824'.split())
+    self.assertError()
 
   def testUnderflowArgs(self):
-    tri = Triangle('1 2 3 4 0 -1073741824'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.ERR)
+    self.triangle = Triangle('1 2 3 4 0 -1073741824'.split())
+    self.assertError()
 
   def testDuplicatePoints1(self):
-    tri = Triangle('-1073741823 1073741823 -1073741823 1073741823 1073741823 1073741823'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.NAT)
+    self.triangle = Triangle('-1073741823 1073741823 -1073741823 1073741823 1073741823 1073741823'.split())
+    self.assertNotATriangle()
 
   def testDuplicatePoints2(self):
-    tri = Triangle('-1073741823 1073741823 -1073741823 1073741823 -1073741823 1073741823'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.NAT)
+    self.triangle = Triangle('-1073741823 1073741823 -1073741823 1073741823 -1073741823 1073741823'.split())
+    self.assertNotATriangle()
 
   def testVerticalLine(self):
-    tri = Triangle('0 -123456789 0 0 0 123456789'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.NAT)
+    self.triangle = Triangle('0 -123456789 0 0 0 123456789'.split())
+    self.assertNotATriangle()
 
   def testHorizontalLine(self):
-    tri = Triangle('-987654321 0 0 0 987654321 0'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.NAT)
+    self.triangle = Triangle('-987654321 0 0 0 987654321 0'.split())
+    self.assertNotATriangle()
 
   def testDiagonalLine1(self):
-    tri = Triangle('0 100000033 100000033 0 200000066 -100000033'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.NAT)
+    self.triangle = Triangle('0 100000033 100000033 0 200000066 -100000033'.split())
+    self.assertNotATriangle()
 
   def testDiagonalLine2(self):
-    tri = Triangle('0 0 -123456789 -123456789 123456789 123456789'.split())
-    tri.build()
-    self.assertEqual(tri.output(), Types.NAT)
+    self.triangle = Triangle('0 0 -123456789 -123456789 123456789 123456789'.split())
+    self.assertNotATriangle()
+
+  # ============================================================================================================
+  # Test Cases Isosceles Triangles
+  # ============================================================================================================
 
   def testIsoscelesAcute(self):
-    tri = Triangle('-1073741822 0 0 1073741823 1073741822 0'.split())
-    tri.build()
-    self.assertEqual(tri.typeA, Types.ISO)
-    self.assertEqual(tri.typeB, Types.ACU)
+    self.triangle = Triangle('-1073741822 0 0 1073741823 1073741822 0'.split())
+    self.assertIsosceles(Types.ACU)
 
   def testIsoscelesRight(self):
-    tri = Triangle('0 1073741823 -1073741823 0 1073741823 0'.split())
-    tri.build()
-    self.assertEqual(tri.typeA, Types.ISO)
-    self.assertEqual(tri.typeB, Types.RIG)
+    self.triangle = Triangle('0 1073741823 -1073741823 0 1073741823 0'.split())
+    self.assertIsosceles(Types.RIG)
 
-  def testIsoscelesObtuse(self):
-    tri = Triangle('0 1073741822 -1073741823 0 1073741823 0'.split())
-    tri.build()
-    self.assertEqual(tri.typeA, Types.ISO)
-    self.assertEqual(tri.typeB, Types.OBT)
+  def testIsoscelesObtuse1(self):
+    self.triangle = Triangle('0 1073741822 -1073741823 0 1073741823 0'.split())
+    self.assertIsosceles(Types.OBT)
+
+  def testIsoscelesObtuse2(self):
+    self.triangle = Triangle('-1073741823 0 0 -1 1073741823 0'.split())
+    self.assertIsosceles(Types.OBT)
+
+  # ============================================================================================================
+  # Test Cases Scalene Triangles
+  # ============================================================================================================
 
   def testScaleneAcute(self):
-    tri = Triangle('-1073741823 -1073741823 -1073741822 1073741822 0 -1073741822'.split())
-    tri.build()
-    self.assertEqual(tri.typeA, Types.SCA)
-    self.assertEqual(tri.typeB, Types.ACU)
+    self.triangle = Triangle('-1073741823 -1073741823 -1073741822 1073741822 0 -1073741822'.split())
+    self.assertScalene(Types.ACU)
 
   def testScaleneRight(self):
-    tri = Triangle('-1073741822 -1073741822 -1073741822 1073741822 0 -1073741822'.split())
-    tri.build()
-    self.assertEqual(tri.typeA, Types.SCA)
-    self.assertEqual(tri.typeB, Types.RIG)
+    self.triangle = Triangle('-1073741822 -1073741822 -1073741822 1073741822 0 -1073741822'.split())
+    self.assertScalene(Types.RIG)
 
-  def testScaleneObtuse(self):
-    tri = Triangle('-1073741822 -1073741821 -1073741822 1073741822 0 -1073741822'.split())
-    tri.build()
-    self.assertEqual(tri.typeA, Types.SCA)
-    self.assertEqual(tri.typeB, Types.OBT)
+  def testScaleneObtuse1(self):
+    self.triangle = Triangle('-1073741822 -1073741821 -1073741822 1073741822 0 -1073741822'.split())
+    self.assertScalene(Types.OBT)
 
-  # arg! what integer points form an equilateral tri ???
+  def testScaleneObtuse2(self):
+    self.triangle = Triangle('-1073741823 -1073741823 -1073741822 -1073741823 1073741823 1073741823'.split())
+    self.assertScalene(Types.OBT)
+
+  # arg! what integer points form an equilateral triangle ???
   # def equilateral(self):
-  #   tri = Triangle('? ? ? ? ? ?'.split())
-  #   tri.build()
-  #   self.assertEqual(tri.typeA, Types.EQU)
-  #   self.assertEqual(tri.typeB, Types.ACU)
+  #   triangle = Triangle('? ? ? ? ? ?'.split())
+  #   triangle.build()
+  #   self.assertEqual(triangle.priType, Types.EQU)
+  #   self.assertEqual(triangle.subType, Types.ACU)
 
 # ==============================================================================================================
 # Triangle Class
@@ -160,8 +188,8 @@ class TestTriangle(unittest.TestCase):
 class Triangle:  
   def __init__(self, pts):
     self.args  = ['./triangle', pts[0], pts[1], pts[2], pts[3], pts[4], pts[5]]
-    self.typeA = None
-    self.typeB = None
+    self.priType = None
+    self.subType = None
 
   def erasePoints(self):
     self.args = ['./triangle', '']
@@ -185,8 +213,8 @@ class Triangle:
   def build(self):
     self.out = self.execTri()
     if len(self.out.split()) == 2:
-      self.typeA = self.out.split()[0]
-      self.typeB = self.out.split()[1]
+      self.priType = self.out.split()[0]
+      self.subType = self.out.split()[1]
 
   def output(self):
     return self.out
