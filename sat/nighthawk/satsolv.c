3eb2b96c7a0804c32c3ac9d97862b712941741d0
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2015-10-21 15:22:20 -0600

    Added main entry point for satsolv.c
    This commit includes an initial skeleton for the main entry point for
    team Nighthawk's SAT solver.

--- sat/nighthawk/satsolv.c
+++ sat/nighthawk/satsolv.c
@@ -2,3 +2,17 @@
  * 
  * <Authors>: Dalton Wallace, Jonathan Whitaker, Jay Tuckett, Tarik Courdy
  */
+
+/* Includes */
+
+/* Definitions */
+#define SATISFIABLE 0
+#define UNSATISFIABLE 1
+#define UNKNOWN 2
+#define ERROR 3
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
