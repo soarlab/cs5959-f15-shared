9c58659321973a1e966c371856e2b08249d13a98
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2015-10-21 15:36:22 -0600

    Implemented boilerplate command line parsing.
    This commit includes an initial implementation of the command line
    parsing and file I/O needed to open the supplied input files.

--- sat/nighthawk/satsolv.c
+++ sat/nighthawk/satsolv.c
@@ -4,15 +4,34 @@
  */
 
 /* Includes */
+#include <stdio.h>
 
 /* Definitions */
 #define SATISFIABLE 0
 #define UNSATISFIABLE 1
 #define UNKNOWN 2
 #define ERROR 3
+#define DEBUG 1
 
 int main(int argc, char **argv)
 {
+    /* if an incorrect number of arguments was supplied, simply
+       print an error and return. */
+    if (argc != 2) {
+        printf("ERROR\n");
+        return 0;
+    }
+
+    /* attempt to open the file containing the logic formula */
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        if (DEBUG)
+            fprintf(stderr, "Can't open input file '%s'!\n", argv[1]);
+
+        printf("ERROR\n");
+        return 0;
+    }
 
     return 0;
 }
